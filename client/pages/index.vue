a79ad1d752161e38c78a9796b9e2e5ebd9599b00
swesweaung-scm <scm.swesweaung@gmail.com>
2022-06-26 00:01:07 +0630

    Docker Set up

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial/>
+  <div>{{ data.name }}</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData({ $axios }) {
+    const data = await $axios.$get(`/api/v1/test`)
+    console.log("data",data)
+    return { data }
+  },
 }
 </script>
